6a4ffeaf575437100ed28c54ae80305ba86f9a80
MelathStofi <melath.stofi@gmail.com>
2019-08-01 09:49:45 +0200

    modify database.sql--> add foreign keys to tables cards and statuses

--- data/database.sql
+++ data/database.sql
@@ -23,6 +23,7 @@ DROP TABLE IF EXISTS public.statuses;
 DROP SEQUENCE IF EXISTS public.statuses_id_seq;
 CREATE TABLE statuses (
     id serial not null ,
+    board_id integer,
     title text
 );
 
@@ -33,6 +34,13 @@ ALTER TABLE ONLY cards
     ADD CONSTRAINT pk_cards_id PRIMARY KEY (id);
 ALTER TABLE ONLY statuses
     ADD CONSTRAINT pk_statuses_id PRIMARY KEY (id);
+
+ALTER TABLE ONLY cards
+    ADD CONSTRAINT fk_board_id FOREIGN KEY (board_id) REFERENCES boards(id) ON DELETE CASCADE ;
+ALTER TABLE ONLY cards
+    ADD CONSTRAINT fk_status_id FOREIGN KEY (status_id) REFERENCES statuses(id) ON DELETE CASCADE ;
+ALTER TABLE ONLY statuses
+    ADD CONSTRAINT fk_board_id FOREIGN KEY (board_id) REFERENCES boards(id) ON DELETE CASCADE ;
 
 
 INSERT INTO boards VALUES (1,'Board 1');
@@ -51,7 +59,7 @@ INSERT INTO cards VALUES (10,2,'planning',2,0);
 INSERT INTO cards VALUES (11,2,'done card 1',3,0);
 INSERT INTO cards VALUES (12,2,'done card 1',3,1);
 
-INSERT INTO statuses VALUES (0,'new');
-INSERT INTO statuses VALUES(1,'in progress');
-INSERT INTO statuses VALUES(2,'testing');
-INSERT INTO statuses VALUES(3,'done');
+INSERT INTO statuses VALUES (0,1,'new');
+INSERT INTO statuses VALUES(1,2,'in progress');
+INSERT INTO statuses VALUES(2,3,'testing');
+INSERT INTO statuses VALUES(3,4,'done');
